baac4f6b531527042de9baa7444ad6f83e9083c0
xypeui <xype.discord@gmail.com>
2023-12-22 01:41:51 +0100

    Add: Metadata for /discord redirect route

--- app/(social)/discord/page.tsx
+++ app/(social)/discord/page.tsx
@@ -1,6 +1,20 @@
 "use client";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  openGraph: {
+    images: [
+      {
+        url: "http://neuronstudios.org/api/discord",
+        width: 1200,
+        height: 630,
+        alt: "Neuron Studios",
+      },
+    ],
+  },
+};
 
 export default function DiscordPage() {
   const { push } = useRouter();
